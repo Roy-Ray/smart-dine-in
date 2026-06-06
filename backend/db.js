58f8e8537167c4e6d949505db3c4ebc79060c9d8
Trisha720 <trisharay2004@gmail.com>
2026-06-06 20:13:37 +0530

    .

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "NewStrong@123", // Replace with your MySQL password
+  password: "trisha@2004", // Replace with your MySQL password
   database: "smart_cafe",
 });
 
